c232f7265fcf805f68b9a22c29ee1731ae5f2808
Leon Mergen <leon@solatis.com>
2014-03-26 09:26:09 +0100

    Use streams2 implementation.

--- examples/deflated-stream.js
+++ examples/deflated-stream.js
@@ -6,11 +6,14 @@ var url  = 'http://ibl.gamechaser.net/f/tagqfxtteucbuldhezkz/bt_level1.gz';
 
 var resp = needle.get(url, { compressed: true, follow: true });
 
-resp.on('data', function(data) {
-  var lines = data.toString().split('\n');
-  console.log('Got ' + lines.length + ' items.');
-  console.log(lines);
-})
+resp.on('readable', function () {
+  var stream = this,
+      chunk  = null;
+
+  while (chunk = stream.read()) {
+    console.log('Got ' + chunk.length + ' bytes');
+  };
+});
 
 resp.on('end', function(data) {
   console.log('Done');
